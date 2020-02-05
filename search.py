0d26224125df511dd5598b9403521a1ffde3f4bb
Jessie Peng <jessie_p@yeah.net>
2020-02-04 23:28:03 -0600

    fix bug in Prim's Algorithm & update sub-optimal heuristic: 2 * MST

--- search.py
+++ search.py
@@ -246,6 +246,7 @@ def astar_multi(maze):
 
     @return path: a list of tuples containing the coordinates of each state in the computed path
     """
+    return extra(maze)
     # state = (position, obj_list)
     # note: all dist are manhattan distance
     # suppose: w >= h
@@ -253,6 +254,114 @@ def astar_multi(maze):
     # if len(obj_list) == 3: heuristic = dist_to_nearest_corner_except_the_middle_one + h + w
     # if len(obj_list) == 2: heuristic = dist_to_nearest_corner + w
     # if len(obj_list) == 1: single point a*
+    # parent = dict()
+    # path = []
+    # pos = maze.getStart()
+    # objs = maze.getObjectives()
+    # objs_tmp = sorted(objs, key=itemgetter(1))
+    # objs = sorted(objs_tmp, key=itemgetter(0))
+    # init_state = (pos, tuple(objs))
+    # cur_state = init_state
+    # dist = dict()
+    # dist[init_state] = 0
+    #
+    # # TODO: A* distance instead of Manhattan distance
+    # # pre-compute pairwise distance of objectives
+    # obj_dist_map = dict()
+    # for i in range(len(objs) - 1):
+    #     for j in range(i + 1, len(objs)):
+    #         obj_dist_map[(objs[i], objs[j])] = abs(objs[i][0] - objs[j][0]) + abs(objs[i][1] - objs[j][1])
+    #         obj_dist_map[(objs[j], objs[i])] = abs(objs[i][0] - objs[j][0]) + abs(objs[i][1] - objs[j][1])
+    #
+    # # Prim's Algorithm for computing Minimum Spanning Tree
+    # # reference source: https://iq.opengenus.org/prim-minimum-spanning-tree-algorithm/
+    # def compute_mst(objs):
+    #     cost = [1000000] * len(objs)
+    #     cost[0] = 0
+    #     parent = [0] * len(objs)
+    #     visited = [False] * len(objs)
+    #     for cnt in range(len(objs)):
+    #         min_cost = 1000000
+    #         min_index = 0
+    #         for u in range(len(objs)):
+    #             if cost[u] < min_cost and not visited[u]:
+    #                 min_cost = cost[u]
+    #                 min_index = u
+    #         visited[min_index] = True
+    #         for v in range(len(objs)):
+    #             if not visited[v] and obj_dist_map[(objs[min_index], objs[v])] < cost[v]:
+    #                 cost[v] = obj_dist_map[(objs[min_index], objs[v])]
+    #                 parent[v] = min_index
+    #     total_cost = 0
+    #     for i in range(1, len(objs)):
+    #         total_cost += obj_dist_map[(objs[i], objs[parent[i]])]
+    #     return total_cost
+    #
+    # # cache MST to boost efficiency
+    # mst = dict()
+    # mst[tuple(objs)] = compute_mst(objs)
+    # heuristic = dict()
+    # heuristic[init_state] = min([abs(pos[0] - objs[i][0]) + abs(pos[1] - objs[i][1])
+    #                              for i in range(4)]) + mst[tuple(objs)]
+    # frontier = []
+    # # tie breaking rule 1: states with fewer objectives have higher priority
+    # # tie breaking rule 2: newer states have higher priority
+    # counter = count()
+    # heappush(frontier, (heuristic[init_state], len(init_state[1]), -next(counter), init_state))
+    # explored = set()
+    # while len(frontier) > 0:
+    #     f, _, _, cur_state = heappop(frontier)
+    #     # if node is previously expanded, current cost must >= previous cost at this node
+    #     if cur_state in explored:
+    #         continue
+    #     # only treat expanded nodes (not nodes in frontier) as visited, since nodes in frontier can be updated
+    #     explored.add(cur_state)
+    #     pos = cur_state[0]
+    #     objs_tuple = cur_state[1]
+    #     objs = list(objs_tuple)
+    #     new_objs = objs
+    #     if pos in new_objs:
+    #         new_objs.remove(pos)
+    #         if len(new_objs) == 0:
+    #             break
+    #     objs_tmp = sorted(objs, key=itemgetter(1))
+    #     new_objs = sorted(objs_tmp, key=itemgetter(0))
+    #     neighbors = maze.getNeighbors(pos[0], pos[1])
+    #     for new_pos in neighbors:
+    #         new_state = (new_pos, tuple(new_objs))
+    #         # pushing expanded node to heap can lead to infinite loop
+    #         if new_state not in explored:
+    #             dist_tmp = dist[cur_state] + 1
+    #             if new_state not in [x[3] for x in frontier]:
+    #                 dist[new_state] = dist_tmp
+    #                 if tuple(new_objs) not in mst:
+    #                     mst[tuple(new_objs)] = compute_mst(new_objs)
+    #                 heuristic[new_state] = min([abs(new_pos[0] - new_objs[i][0]) + abs(new_pos[1] - new_objs[i][1])
+    #                                             for i in range(len(objs))]) + mst[tuple(new_objs)]
+    #                 heappush(frontier, (dist[new_state] + heuristic[new_state], len(new_state[1]), -next(counter), new_state))
+    #                 parent[new_state] = cur_state
+    #             # can update node in frontier if distance is shorter
+    #             elif dist_tmp < dist[new_state]:
+    #                 dist[new_state] = dist_tmp
+    #                 heappush(frontier, (dist[new_state] + heuristic[new_state], len(new_state[1]), -next(counter), new_state))
+    #                 parent[new_state] = cur_state
+    # while cur_state != init_state:
+    #     path.append(cur_state[0])
+    #     cur_state = parent[cur_state]
+    # path.append(cur_state[0])
+    # path.reverse()
+    # return path
+
+
+def extra(maze):
+    """
+    Runs extra credit suggestion.
+
+    @param maze: The maze to execute the search on.
+
+    @return path: a list of tuples containing the coordinates of each state in the computed path
+    """
+    # Modified heuristic: MST -> 2 * MST
     parent = dict()
     path = []
     pos = maze.getStart()
@@ -267,9 +376,10 @@ def astar_multi(maze):
     # TODO: A* distance instead of Manhattan distance
     # pre-compute pairwise distance of objectives
     obj_dist_map = dict()
-    for x in objs:
-        for y in objs:
-            obj_dist_map[(x, y)] = abs(x[0] - y[0]) + abs(x[1] - y[1])
+    for i in range(len(objs) - 1):
+        for j in range(i + 1, len(objs)):
+            obj_dist_map[(objs[i], objs[j])] = abs(objs[i][0] - objs[j][0]) + abs(objs[i][1] - objs[j][1])
+            obj_dist_map[(objs[j], objs[i])] = abs(objs[i][0] - objs[j][0]) + abs(objs[i][1] - objs[j][1])
 
     # Prim's Algorithm for computing Minimum Spanning Tree
     # reference source: https://iq.opengenus.org/prim-minimum-spanning-tree-algorithm/
@@ -278,18 +388,18 @@ def astar_multi(maze):
         cost[0] = 0
         parent = [0] * len(objs)
         visited = [False] * len(objs)
-        for u in range(len(objs)):
+        for cnt in range(len(objs)):
             min_cost = 1000000
             min_index = 0
-            for v in range(len(objs)):
-                if cost[v] < min_cost and not visited[v]:
-                    min_cost = cost[v]
-                    min_index = v
+            for u in range(len(objs)):
+                if cost[u] < min_cost and not visited[u]:
+                    min_cost = cost[u]
+                    min_index = u
             visited[min_index] = True
             for v in range(len(objs)):
-                if obj_dist_map[(objs[u], objs[v])] < cost[v] and not visited[v]:
-                    cost[v] = obj_dist_map[(objs[u], objs[v])]
-                    parent[v] = u
+                if not visited[v] and obj_dist_map[(objs[min_index], objs[v])] < cost[v]:
+                    cost[v] = obj_dist_map[(objs[min_index], objs[v])]
+                    parent[v] = min_index
         total_cost = 0
         for i in range(1, len(objs)):
             total_cost += obj_dist_map[(objs[i], objs[parent[i]])]
@@ -300,7 +410,7 @@ def astar_multi(maze):
     mst[tuple(objs)] = compute_mst(objs)
     heuristic = dict()
     heuristic[init_state] = min([abs(pos[0] - objs[i][0]) + abs(pos[1] - objs[i][1])
-                                 for i in range(4)]) + mst[tuple(objs)]
+                                 for i in range(4)]) + mst[tuple(objs)] * 2
     frontier = []
     # tie breaking rule 1: states with fewer objectives have higher priority
     # tie breaking rule 2: newer states have higher priority
@@ -335,7 +445,7 @@ def astar_multi(maze):
                     if tuple(new_objs) not in mst:
                         mst[tuple(new_objs)] = compute_mst(new_objs)
                     heuristic[new_state] = min([abs(new_pos[0] - new_objs[i][0]) + abs(new_pos[1] - new_objs[i][1])
-                                                for i in range(len(objs))]) + mst[tuple(new_objs)]
+                                                for i in range(len(objs))]) + mst[tuple(new_objs)] * 2
                     heappush(frontier, (dist[new_state] + heuristic[new_state], len(new_state[1]), -next(counter), new_state))
                     parent[new_state] = cur_state
                 # can update node in frontier if distance is shorter
@@ -348,46 +458,4 @@ def astar_multi(maze):
         cur_state = parent[cur_state]
     path.append(cur_state[0])
     path.reverse()
-    return path
-
-
-def extra(maze):
-    """
-    Runs extra credit suggestion.
-
-    @param maze: The maze to execute the search on.
-
-    @return path: a list of tuples containing the coordinates of each state in the computed path
-    """
-    # Greedy: find nearest objective using bfs one by one
-    objs = maze.getObjectives()
-    start = maze.getStart()
-    cur = start
-    path = [start]
-    for j in range(len(objs)):
-        frontier = deque()
-        frontier.append(start)
-        visited = set()
-        visited.add(start)
-        tmp_path = []
-        parent = dict()
-        while len(frontier) > 0:
-            cur = frontier.popleft()
-            if cur in objs:
-                objs.remove(cur)
-                break
-            neighbors = maze.getNeighbors(cur[0], cur[1])
-            for i in neighbors:
-                if i not in visited:
-                    parent[i] = cur
-                    frontier.append(i)
-                    visited.add(i)
-        tmp_start = cur
-        while cur != start:
-            tmp_path.append(cur)
-            cur = parent[cur]
-        tmp_path.reverse()
-        for p in tmp_path:
-            path.append(p)
-        start = tmp_start
     return path
